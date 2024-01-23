5535fbdc1bd89c59819c0027765c9153eb8f666c
mostafa farrag <mostafa.kz@outlook.sa>
2024-01-24 00:51:32 +0100

    update booking service

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Booking } from './booking';
+import { Booking, BookingDocument } from './booking';
 import { Model } from 'mongoose';
 
 @Injectable()
@@ -8,5 +8,55 @@ export class BookingService {
     constructor(
         @InjectModel(Booking.name)
         private readonly bookingModel: Model<Booking>
-    ) {}
+    ) { }
+
+    /*create(by hotel_id,room_id, guest_id),findAll()
+    findAllByHotelId,findAllByRoomId,findOneById, findAllByguestId, findOneByguestId, updateOne,removeOne,*/
+
+    async create(hotel_id: any, room_id: any, guest_id: any, data: any) {
+        data.hotel = hotel_id
+        data.room = room_id
+        data.guest = guest_id
+        return await this.bookingModel.create(data)
+    }
+
+    async findAll() {
+        return this.bookingModel.find().exec()
+    }
+    async findOneById(_id) {
+        return this.bookingModel.findById(_id)
+    }
+
+    async findAllByHotelId(hotel_id) {
+        return this.bookingModel.find({ hotel: hotel_id }).exec()
+    }
+
+
+    async findAllByRoomId(room_id) {
+        return this.bookingModel.find({ room: room_id }).exec()
+    }
+
+    async findAllByGuestId(guest_id) {
+        return this.bookingModel.find({ guest: guest_id }).exec()
+    }
+
+
+    async updateOne(_id: any, data: any) {
+        return this.bookingModel.findByIdAndUpdate(_id, data, { new: true, })
+    }
+    async removeOne(_id: any) {
+        return this.bookingModel.findByIdAndDelete(_id)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
