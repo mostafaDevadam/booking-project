d4a633eaa1ca47a3320ee4ab115e148ce2dfdda9
mostafa farrag <mostafa.kz@outlook.sa>
2024-02-17 18:56:41 +0100

    update dashboard with use chart-component

--- src/app/hotel/dashboard/dashboard.module.ts
+++ src/app/hotel/dashboard/dashboard.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -7,14 +7,28 @@ import { IonicModule } from '@ionic/angular';
 import { DashboardPageRoutingModule } from './dashboard-routing.module';
 
 import { DashboardPage } from './dashboard.page';
+import { ChartComponent } from 'src/app/shared/components/chart/chart.component';
+import { FusionChartsModule } from 'angular-fusioncharts';
+import * as Charts from "fusioncharts/fusioncharts.charts";
+import * as FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+import * as FusionCharts from "fusioncharts";
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { HighchartsChartModule } from 'highcharts-angular';
+
+
+//FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme);
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    DashboardPageRoutingModule
+    DashboardPageRoutingModule,
+    //FusionChartsModule,
+    NgxChartsModule,
+    HighchartsChartModule,
   ],
-  declarations: [DashboardPage]
+  declarations: [DashboardPage, ChartComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class DashboardPageModule {}
+export class DashboardPageModule { }
--- src/app/hotel/dashboard/dashboard.page.ts
+++ src/app/hotel/dashboard/dashboard.page.ts
@@ -8,6 +8,112 @@ import { HotelService } from 'src/app/services/hotel.service';
 })
 export class DashboardPage implements OnInit {
 
+  roomsDataForChart = {
+    type: 'column',
+    name: 'Rooms',
+    data: [
+      {
+        //x: 0,
+        y: 9,
+        name: "Available Rooms",
+        color: "#00FF00",
+
+      }, {
+        //x: 0,
+        y: 6,
+        name: "Booked Rooms",
+        color: "#FF00FF"
+      }
+    ],
+  }
+
+  roomsTypeDataForChart = {
+    type: 'pie',
+    name: 'RoomsTypes',
+    data: [
+      {
+        //x: 0,
+        y: 9,
+        name: "Single Rooms",
+        color: "#00FF00",
+
+      }, {
+        //x: 0,
+        y: 6,
+        name: "Double Rooms",
+        color: "#FF00FF"
+      }
+    ],
+  }
+
+  guestsDataForChart = {
+    type: 'column',
+    name: 'Guests',
+    data: [
+      {
+        //x: 0,
+        y: 500,
+        name: "Guests",
+        color: "#00FF00",
+
+      },
+
+      {
+        //x: 0,
+        y: 200,
+        name: "Active Guests",
+        color: "#FF00FF"
+      },
+      {
+        //x: 0,
+        y: 300,
+        name: "InActive",
+        color: "#F26D5B",
+
+      },
+    ],
+  }
+
+  //  cleaned-,-not-cleaned-rooms,
+  cleanRoomsDataForChart = {
+    type: 'column',
+    name: 'CleanRooms',
+    data: [
+      {
+        //x: 0,
+        y: 9,
+        name: "Cleaned Rooms ",
+        color: "#00FF00",
+
+      }, {
+        //x: 0,
+        y: 3,
+        name: "Not-Cleaned Rooms",
+        color: "#FF00FF"
+      }
+    ],
+  }
+
+  // incomes
+  incomesDataForChart = {
+    type: 'column',
+    name: 'InComes',
+    data: [
+      {
+        //x: 0,
+        y: 10000,
+        name: "Low incomes",
+        color: "#00FF00",
+
+      }, {
+        //x: 0,
+        y: 17000,
+        name: "Good incomes",
+        color: "#FF00FF"
+      }
+    ],
+  }
+
   constructor(
     private hotelService: HotelService
   ) { }
